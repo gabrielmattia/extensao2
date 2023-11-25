ddad0876d66f5b82835fb5bf9fc943674daa03df
gabrielmattia <61206299+gabrielmattia@users.noreply.github.com>
2023-11-25 10:47:43 -0400

    Delete .output directory

--- .output/server/chunks/app/_nuxt/todososprojetos-styles.fccf406d.mjs
+++
@@ -1,6 +0,0 @@
-const todososprojetos_vue_vue_type_style_index_0_scoped_73d4ec11_lang = ".post[data-v-73d4ec11]{background:#fff;transition:box-shadow .1s ease-in}.post[data-v-73d4ec11]:hover{box-shadow:1px 1px 3px #aaa}.post+.post[data-v-73d4ec11]{margin-top:2rem}";
-
-const todososprojetosStyles_fccf406d = [todososprojetos_vue_vue_type_style_index_0_scoped_73d4ec11_lang, todososprojetos_vue_vue_type_style_index_0_scoped_73d4ec11_lang];
-
-export { todososprojetosStyles_fccf406d as default };
-//# sourceMappingURL=todososprojetos-styles.fccf406d.mjs.map
