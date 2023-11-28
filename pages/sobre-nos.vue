f4feb1f44e4434cfe29d3140b0c6f931a17018f9
vieiramanda <amandavc27@gmail.com>
2023-11-27 22:36:12 -0400

    updated about us page

--- pages/sobre-nos.vue
+++ pages/sobre-nos.vue
@@ -3,8 +3,33 @@
 <template>
   <NuxtLayout name="basic">
     <template #main-content>
-      <div class="text-center">
-        <h3>Página em construção...</h3>
+      <div class="lg:px-[8rem] text-justify">
+        <h3 class="mb-3">Sobre o projeto</h3>
+        <p class="mb-3 leading-5">
+          O projeto de extensão desenvolvido na UFMS (Universidade Federal do Mato Grosso do Sul) tem o objetivo de produzir um website para publicar projetos
+          arquitetônicos de habitação coletiva. Os exemplares publicizados foram identificados, coletados, catalogados e
+          analisados em seis pesquisas de iniciação científicas anteriores, orientadas por Felipe Anitelli, desde 2020.
+          Eles estavam organizados em um banco de dados que contém centenas de casos, mas que, até agora, estavam em uma
+          pasta digital de acesso privado, no Google Drive.
+        </p>
+        <p class="mb-3 leading-5">
+          A ideia foi selecionar casos expressivos desta amostra e publicar no website: peças gráficas e informações
+          gerais dos empreendimentos. Disponibilizar amostras de projeto é um importante recurso para estudar essa
+          tipologia habitacional, que está disponível para consulta de alunos, professores, pesquisadores,
+          profissionais, cidadãos, etc...
+        </p>
+        <p class="mb-3 leading-5">
+          Foram quatro eixos principais que direcionaram o trabalho realizado: treinamento, diagramação, postagem e
+          divulgação. As tarefas se dividiram entre demandas técnicas e estéticas, ou seja, (1) identificar e estudar
+          recursos digitais adequados para produzir o website e (2) tornar a diagramação das páginas um elemento para
+          potencializar o acesso ao sítio. Por causa da abordagem adotada, foram selecionados alunos de graduação em
+          Arquitetura e Urbanismo e Ciência da Computação para formar a equipe executora.
+        </p>
+        <p class="font-semibold">Colaboradores:</p>
+        <p>Felipe Anitelli</p>
+        <p>Amanda Vieira Costa</p>
+        <p>Wellington Gabriel de Mattia</p>
+        <p>Bruna Costa Dias</p>
       </div>
     </template>
   </NuxtLayout>
