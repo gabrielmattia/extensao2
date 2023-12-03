eefd740be4172bb381a9b690d4a147e594251322
vieiramanda <amandavc27@gmail.com>
2023-12-02 22:14:35 -0400

    implemented random projects to show on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,18 @@
-<script setup></script>
+<script setup>
+  const { data: projectsData, pending, error } = await useFetch(() => '/api/projetos')
+
+  const mainProjects = computed(() => {
+    let randomProjects = []
+    while (randomProjects.length < 4) {
+      const item = projectsData.value[Math.floor(Math.random() * projectsData.value.length)]
+      randomProjects.push(projectsData.value.find((p) => p?.id === item.id))
+      const index = projectsData.value.indexOf(item)
+      projectsData.value.splice(index, 1)
+    }
+
+    return randomProjects
+  })
+</script>
 
 <template>
   <NuxtLayout name="basic">
@@ -11,8 +25,26 @@
             class="w-full h-full max-w-full rounded-lg"
           />
         </div>
-        <div class="">
-          <MainProjects />
+
+        <div
+          v-if="mainProjects.length > 0"
+          class="welcome-content"
+        >
+          <ClientOnly>
+            <CardProject
+              v-for="proj in mainProjects"
+              :key="proj.id"
+              :projectId="proj.id"
+              :projectTitle="proj.nome_oficial"
+              :year="proj?.ano"
+              :author="proj?.autor"
+              :country="proj?.pais"
+              :state="proj?.estado"
+              :city="proj?.cidade"
+              :imageFileName="proj.imagens[0]?.file"
+              :imgAltText="proj.imagens[0]?.name"
+            />
+          </ClientOnly>
         </div>
       </div>
     </template>
@@ -21,6 +53,9 @@
 
 <style scoped>
   .presentation-wrapper {
-    @apply grid grid-cols-1 xl:grid-cols-2 gap-5 content-center;
+    @apply grid grid-cols-1 xl:grid-cols-2 xl:pt-8 gap-5 content-center;
+  }
+  .welcome-content {
+    @apply w-full grid grid-cols-1 md:grid-cols-custom-2 justify-items-center md:place-content-center gap-4;
   }
 </style>
