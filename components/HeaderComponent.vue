39581b26f04ed53b877899bd9597ad084ef1ad9a
Wellington Gabriel <bielsmi03@gmail.com>
2023-12-08 13:03:58 -0400

    v1.2.0

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -2,61 +2,39 @@
 
 <template>
   <header>
-    <div class="container lg:max-w-[1024px] mx-auto px-4 lg:px-14 xl:px-20">
+    <div class="container lg:max-w-[900px] mx-auto px-4 lg:px-14 xl:px-20">
       <div class="flex flex-col justify-center">
         <div class="flex flex-col items-center md:items-baseline">
           <h1 class="text-center md:text-left pl-2">DEBATES SOBRE HABITAÇÃO COLETIVA</h1>
           <p class="text-center md:text-left pl-3">
-            Debate e publicação de projetos arquitetônicos de habitação coletiva realizados no Brasil.
+            DEBATE E PUBLICAÇÃO DE PROJETOS ARQUITETÔNICOS DE HABITAÇÃO COLETIVA REALIZADOS NO BRASIL.
           </p>
         </div>
 
-        <div class="navbar justify-between min-h-[3rem] p-0">
+        <div class="navbar justify-between min-h-[3rem] p-0 ">
           <div class="flex">
-            <NuxtLink
-              to="/"
-              class="btn btn-ghost btn-sm"
-            >
+            <NuxtLink to="/" class="btn btn-ghost btn-sm">
               Início
             </NuxtLink>
-            <NuxtLink
-              to="/projetos"
-              class="btn btn-ghost btn-sm"
-            >
+            <NuxtLink to="/projetos" class="btn btn-ghost btn-sm">
               Projetos
             </NuxtLink>
-            <NuxtLink
-              to="/sobre-nos"
-              class="btn btn-ghost btn-sm"
-            >
+            <NuxtLink to="/sobre-nos" class="btn btn-ghost btn-sm">
               Sobre
             </NuxtLink>
           </div>
+
           <div class="flex-end">
             <div class="bg-ig-logo text-white self-end rounded-lg p-0.5 md:mb-1 md:hover:opacity-70 m-2">
-              <a
-                href="https://www.instagram.com/debates.habitacao.coletiva/?igshid=NzZlODBkYWE4Ng%3D%3D"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="md:hover:opacity-100"
-              >
-                <img
-                  src="../assets/images/instagram-logo.svg"
-                  alt="instagram link"
-                />
+              <a href="https://www.instagram.com/debates.habitacao.coletiva/?igshid=NzZlODBkYWE4Ng%3D%3D" target="_blank"
+                rel="noopener noreferrer" class="md:hover:opacity-100">
+                <img src="../assets/images/instagram-logo.svg" alt="instagram link" />
               </a>
             </div>
             <div class="bg-yt-logo text-white self-end rounded-lg p-0.5 md:mb-1 md:hover:opacity-70 m-2">
-              <a
-                href="https://www.youtube.com/@debateshabitacaocoletiva"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="md:hover:opacity-100"
-              >
-                <img
-                  src="../assets/images/youtube-logo.svg"
-                  alt="youtube link"
-                />
+              <a href="https://www.youtube.com/@debateshabitacaocoletiva" target="_blank" rel="noopener noreferrer"
+                class="md:hover:opacity-100">
+                <img src="../assets/images/youtube-logo.svg" alt="youtube link" />
               </a>
             </div>
           </div>
@@ -67,26 +45,26 @@
 </template>
 
 <style scoped>
-  header {
-    @apply bg-base-300 py-4;
-  }
+header {
+  @apply bg-base-300 py-4;
+}
 
-  .bg-yt-logo {
-    background: #f61c0d;
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f09433', endColorstr='#bc1888', GradientType=1);
-  }
+.bg-yt-logo {
+  background: #f61c0d;
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f09433', endColorstr='#bc1888', GradientType=1);
+}
 
-  .bg-ig-logo {
-    background: #f09433;
-    background: -moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
-    background: -webkit-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
-    background: linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f09433', endColorstr='#bc1888', GradientType=1);
-  }
+.bg-ig-logo {
+  background: #f09433;
+  background: -moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
+  background: -webkit-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
+  background: linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f09433', endColorstr='#bc1888', GradientType=1);
+}
 
-  @media (max-width: 767px) {
-    a {
-      @apply min-h-[1rem] h-[1rem] max-h-[1rem];
-    }
+@media (max-width: 767px) {
+  a {
+    @apply min-h-[1rem] h-[1rem] max-h-[1rem];
   }
+}
 </style>
